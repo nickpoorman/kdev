07f1d4821eb79477fd3bac1387b04be3b3db9558
Nick Poorman <nickpoorman@gmail.com>
2013-10-10 21:41:44 -0400

    adding http rest api

---
+++ README.md
@@ -0,0 +1,7 @@
+#Notification System
+
+##Environment Variables
+ * MYSQL_HOST = MYSQL Host Address
+ * MYSQL_USER = MYSQL Username
+ * MYSQL_PASS = MYSQL Password
+ * MYSQL_DATABASE = MYSQL Database Name
---
+++ http-rest-api.js
@@ -0,0 +1,237 @@
+/**
+ * Module dependencies
+ */
+var app = express(),
+  expressValidator = require('express-validator'),
+  Notification = require('./lib/notification'),
+  redis = require("redis");
+
+/**
+ * The redis client used to get/set session data.
+ */
+var redisSessionClient = redis.createClient();
+
+
+/**
+ * Create the servers
+ */
+var server = require('http').createServer(app); // create the express.js server
+
+/**
+ * MYSQL Database
+ */
+var dbConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER || 'nick',
+  password: process.env.MYSQL_PASS || 'testpassword000',
+  database: process.env.MYSQL_DATABASE || 'kdev';
+};
+var db = require('./lib/db')(dbConfig);
+
+/**
+ * App configuration.
+ */
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set("views", __dirname + "/views");
+  app.set('views', __dirname);
+  app.set('view engine', 'jade');
+
+  // make sure this is above the logger so to
+  // not log all the server status messages
+  app.get("/server/status", function(req, res) {
+    return res.type('txt').send('online');
+  });
+
+  // make sure this is above the logger so to
+  // not log all the static asset requests
+  app.use(express.static(__dirname + '/public'));
+
+  // log level depending on run mode
+  if ('production' == process.env.NODE_ENV) {
+    app.use(express.logger());
+  } else {
+    app.use(express.logger('dev'));
+  }
+
+  app.use(express.compress());
+  app.use(express.bodyParser());
+  app.use(expressValidator());
+  app.use(express.methodOverride());
+  app.use(app.router);
+
+});
+
+/**
+ * Tell express to start listening.
+ */
+server.listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
+});
+
+/**
+ * HTTP Notifications API
+ * All requests to the API should provide a `sessionID`
+ *  to run the query in the context of a user.
+ */
+app.param('sessionID', function(req, res, next, id) {
+  // hit redis for the session
+  redisSessionClient.get(id, function(err, session) {
+    // reply is null when the key is missing
+    if (!session) {
+      // send them an error
+      return res.json(401, {
+        msg: "Session not found for sessionID: " + id
+      });
+    }
+    req.session = session;
+  });
+});
+
+/**
+ * Handle create Notification requests to the API.
+ *           query params, ?=
+ * @param {int} sessionID  The sessionID to run the query in the context of a user
+ *
+ *           function callback params
+ * @param  {Object}   req  The request object express provides.
+ * @param  {Object}   res  The response object express provides.
+ * @param  {Function} next An optional callback to the next middleware.
+ *
+ * @returns {JSON} Sends back the id of the created Notification in the response.
+ */
+app.post('/notifications', function(req, res, next) {
+  // TODO: determine proper validations for this resource
+
+  // save the object
+  var n = new Notification(req.body);
+  n.create(function(err, id) {
+    if (err) return next(err);
+
+    return res.json({
+      id: id
+    })
+  });
+});
+
+/**
+ * Handle show Notification requests to the API.
+ *           query params, ?=
+ * @param {int} sessionID  The sessionID to run the query in the context of a user
+ * @param {int} id The ID of the Notification to query.
+ *
+ *           function callback params
+ * @param  {Object}   req  The request object express provides.
+ * @param  {Object}   res  The response object express provides.
+ * @param  {Function} next An optional callback to the next middleware.
+ *
+ * @returns {JSON} Sends back the Notifications matching the given id in the response.
+ */
+app.get('/notifications/:id', function(req, res, next) {
+  // TODO: determine proper validations for this resource
+
+  // get the object based on the id
+  var n = new Notification(req.body);
+  n.get(req.param('id'), function(err, results) {
+    if (err) return next(err);
+
+    return res.json({
+      results: results
+    })
+  });
+});
+
+/**
+ * Handle index Notification requests to the API.
+ *           query params, ?=
+ * @param {int} sessionID  The sessionID to run the query in the context of a user
+ * @param {int} start The offset to start the query, ie. for pagination. Default is 0.
+ * @param {int} limit Limit the number of results returned. Default is 0.
+ *
+ *           function callback params
+ * @param  {Object}   req  The request object express provides.
+ * @param  {Object}   res  The response object express provides.
+ * @param  {Function} next An optional callback to the next middleware.
+ *
+ * @returns {JSON} Sends back the Notifications for the given user in the response.
+ */
+app.get('/notifications', function(req, res, next) {
+  // TODO: determine proper validations for this resource
+
+  if (!res.session) {
+    return res.json(401, {
+      msg: "No session found."
+    });
+  }
+
+  var opts = {};
+  if (req.param('start')) opts.start = req.param('start');
+  if (req.param('limit')) opts.limit = req.param('limit');
+
+  // get all the notifications for the user
+  var n = new Notification(req.body);
+  n.index(opts, function(err, results) {
+    if (err) return next(err);
+
+    return res.json({
+      results: results
+    })
+  });
+});
+
+
+/**
+ * Handle delete Notification requests to the API.
+ *           query params, ?=
+ * @param {int} sessionID  The sessionID to run the query in the context of a user
+ * @param {int} start The offset to start the query, ie. for pagination. Default is 0.
+ * @param {int} limit Limit the number of results returned. Default is 0.
+ * @param {int} id The ID of the Notification to query.
+ *
+ *           function callback params
+ * @param  {Object}   req  The request object express provides.
+ * @param  {Object}   res  The response object express provides.
+ * @param  {Function} next An optional callback to the next middleware.
+ *
+ * @returns {JSON} Sends back the Notifications for the given user in the response.
+ */
+app.delete('/notifications/:id', function(req, res, next) {
+  // TODO: determine proper validations for this resource
+
+  // delete the notification based on the id
+  var n = new Notification(req.body);
+  n.get(req.param('id'), function(err, count) {
+    if (err) return next(err);
+
+    return res.json({
+      count: count
+    })
+  });
+});
+
+/**
+ * Handle set Notification requests to the API.
+ *           query params, ?=
+ * @param {int} sessionID  The sessionID to run the query in the context of a user
+ *
+ *           function callback params
+ * @param  {Object}   req  The request object express provides.
+ * @param  {Object}   res  The response object express provides.
+ * @param  {Function} next An optional callback to the next middleware.
+ *
+ * @returns {JSON} Sends back the Notifications matching the given id in the response.
+ */
+app.put('/notifications/:id', function(req, res, next) {
+  // TODO: determine proper validations for this resource
+
+  // get the object based on the id
+  var n = new Notification(req.body);
+  n.get(req.param('id'), function(err, results) {
+    if (err) return next(err);
+
+    return res.json({
+      results: results
+    })
+  });
+});
